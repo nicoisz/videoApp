a73b3626c6a3e115b29b80e69f882a6535fd04ba
nicoisz <nicolasignacio.sz@gmail.com>
2023-05-29 22:29:41 -0400

    first steps

---
+++ lib/domain/entities/video_post.dart
@@ -0,0 +1,12 @@
+class VideoPost {
+  final String caption;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  VideoPost(
+      {required this.caption,
+      required this.videoUrl,
+      required this.likes,
+      required this.views});
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:videoapp/config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
+
